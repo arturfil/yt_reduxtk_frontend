5df830a8a476c0c5a765c4ae0de4cabf7f5b8e8b
arturfil <arturfil@hotmail.com>
2022-04-23 16:50:03 -0400

    changed the cards

--- src/features/games/GamesPage.tsx
+++ src/features/games/GamesPage.tsx
@@ -7,18 +7,18 @@ export default function GamesPage() {
 
   return (
     <Container>
-      <Grid container >
+      <Grid container sx={{marginTop: 5}} >
         {games &&
           games.map((game) => (
-            <Grid item key={game._id} xs={4} sx={{
+            <Grid item key={game._id} xs={4} >
+              <Grid sx={{
                 borderRadius: 2,
                 margin: 2,
                 padding: 3,
                 backgroundImage: 'linear-gradient(90deg, rgb(12, 237, 147), rgb(13, 200, 220))',
                 minHeight: 50,
                 minWidth: 50,
-            }}>
-              <h4>{game.name ? game.name : "No name"}</h4>
+            }}>{game.name ? game.name : "No name"}</Grid>
             </Grid>
           ))}
       </Grid>
